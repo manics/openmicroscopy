d16c10fc38a94b718e3806f44e00e94542644851
Simon Li <spli@dundee.ac.uk>
2018-11-21 10:19:39 +0000

    Reuse existing console log. Can have multiple default loggers

--- components/tools/OmeroWeb/omeroweb/settings.py
+++ components/tools/OmeroWeb/omeroweb/settings.py
@@ -130,18 +130,12 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'formatter': 'full_request',
         },
-        'default_console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard',
-        },
         'null': {
             'level': 'DEBUG',
             'class': 'django.utils.log.NullHandler',
         },
         'console': {
             'level': 'DEBUG',
-            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
             'formatter': 'standard'
         },
@@ -347,13 +341,13 @@ CUSTOM_SETTINGS_MAPPINGS = {
          ("A boolean that turns on/off debug mode. "
           "Use debug mode only in development, not in production, as it logs "
           "sensitive and confidential information in plaintext.")],
-    "omero.web.default_log_handler_name":
-        ["LOG_HANDLER_NAME",
-         "default",
-         str,
-         ("Name of a built-in log handler, either `default` for the default "
-          "rotating file handler or `default_console` to log to stdout, for "
-          "example when running in the foreground "
+    "omero.web.default_log_handlers":
+        ["DEFAULT_LOG_HANDLERS",
+         "[\"default\"]",
+         json.loads,
+         ("Names of built-in log handlers, such as `\"default\"` for the "
+          "default rotating file handler or `\"console\"` to log to stdout, "
+          "for example when running in the foreground "
           "(`omero web start --foreground`)")],
     "omero.web.secret_key":
         ["SECRET_KEY",
@@ -969,7 +963,7 @@ process_custom_settings(sys.modules[__name__], 'CUSTOM_SETTINGS_MAPPINGS',
                         'DEPRECATED_SETTINGS_MAPPINGS')
 process_custom_settings(sys.modules[__name__], 'DEVELOPMENT_SETTINGS_MAPPINGS')
 
-LOGGING['loggers']['']['handlers'] = [sys.modules[__name__].LOG_HANDLER_NAME]
+LOGGING['loggers']['']['handlers'] = sys.modules[__name__].DEFAULT_LOG_HANDLERS
 
 
 if not DEBUG:  # from CUSTOM_SETTINGS_MAPPINGS  # noqa
